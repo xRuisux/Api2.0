72090013ebc2e6fde4a759636a18ed6d712e820c
Luiz Lima <luiz.c.brito1@gmail.com>
2018-10-29 15:20:26 -0300

    renomeando funcao no pup

--- server/service/puppeter.js
+++ server/service/puppeter.js
@@ -6,7 +6,8 @@ const Promo = require('../api/crawler/crawModel');
 mongoose.connect('mongodb://localhost/desafiodb', { useNewUrlParser: true });
 const promosArray = [];
 
-const teste = Promo.find({}, 'link title', (err, promos) => {
+
+const linksFind = Promo.find({}, 'link title', (err, promos) => {
     if (err) {
         console.log(err);
     } else {
@@ -16,29 +17,29 @@ const teste = Promo.find({}, 'link title', (err, promos) => {
         return promosArray;
     };
 });
-
 const pup = async (promos, Pup, browser) => {
-        (async () => {
-            try {
-                const page = await  browser.newPage();
-                await page.goto(promos.link, {waitUntil: 'networkidle2'});
-                await page.pdf({path: `../../src/${promos.title}.pdf`, format: 'A4'});                           
-            } catch (error) {
-                console.log(error);
-            }            
-            // pdfs.push(createDoc(Pup, promo));
-        })();
+    
+    (async () => {
+        try {
+            const page = await  browser.newPage();
+            await page.goto(promos.link, {waitUntil: 'networkidle2'});
+            await page.pdf({path: `../../src/${promos.title}.pdf`, format: 'A4'});                           
+        } catch (error) {
+            console.log(error);
+        }            
+        // pdfs.push(createDoc(Pup, promo));
+    })();
 };
 
 const run =  async () =>{
-    teste.then(async()=>{
-        //console.log (promosArray); 
-        const browser = await puppeteer.launch();
-        promosArray.forEach (element => {
-            pup(element, Pup, browser);                  
-        });      
-    });
-    await browser.close(); 
+linksFind.then(async()=>{
+    //console.log (promosArray); 
+    const browser = await puppeteer.launch();
+    promosArray.forEach (element => {
+        pup(element, Pup, browser);                  
+    });      
+});
+await browser.close(); 
 };
 // run();
 module.exports = run;
