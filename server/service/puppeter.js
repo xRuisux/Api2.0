815af4cdb4fdf5838bd9369016a1b1d5d94fc90a
Luiz Lima <luiz.c.brito1@gmail.com>
2018-10-29 15:16:50 -0300

    ajustes no pup

--- server/service/puppeter.js
+++ server/service/puppeter.js
@@ -6,7 +6,6 @@ const Promo = require('../api/crawler/crawModel');
 mongoose.connect('mongodb://localhost/desafiodb', { useNewUrlParser: true });
 const promosArray = [];
 
-
 const teste = Promo.find({}, 'link title', (err, promos) => {
     if (err) {
         console.log(err);
@@ -18,41 +17,28 @@ const teste = Promo.find({}, 'link title', (err, promos) => {
     };
 });
 
-const pup = async (promos, Pup, page, browser) => {
-    let pdfs = [];
-    let promo = {};
+const pup = async (promos, Pup, browser) => {
         (async () => {
             try {
-                const browser = await puppeteer.launch();
                 const page = await  browser.newPage();
                 await page.goto(promos.link, {waitUntil: 'networkidle2'});
-                await page.pdf({path: `../../src/${promos.title}.pdf`, format: 'A4'});
-                await browser.close();  
-                   
+                await page.pdf({path: `../../src/${promos.title}.pdf`, format: 'A4'});                           
             } catch (error) {
                 console.log(error);
-            }
-            
+            }            
             // pdfs.push(createDoc(Pup, promo));
         })();
 };
 
-
-
 const run =  async () =>{
     teste.then(async()=>{
         //console.log (promosArray); 
-
-        
+        const browser = await puppeteer.launch();
         promosArray.forEach (element => {
-            pup(element, Pup);
-                    
-        });
-           
-        
-        
+            pup(element, Pup, browser);                  
+        });      
     });
-    
+    await browser.close(); 
 };
-run();
-// module.exports = run;
+// run();
+module.exports = run;
